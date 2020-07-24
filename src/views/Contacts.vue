651e3267a8ceb9472e515bec77ca8fcf754f3d56
Boardonly <dr.yorik@gmail.com>
2020-07-24 20:07:04 +0300

    first commit

---
+++ src/views/Contacts.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="vpl-contacts">
+    <h1>This is Contacts page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contacts"
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="vlp-home">
+    <Search />
+    <v-cataloge />
+    <Events />
+	 <Interest />
+	 <Events />
+    <CatalogeMob />
+    <v-footer />
+  </div>
+  <!-- /.vlp-home -->
+</template>
+
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import Search from "@/components/home/Search.vue";
+import Cataloge from "@/components/home/Cataloge.vue";
+import Interest from "@/components/home/Interest.vue";
+import Events from "@/components/home/Events.vue";
+import Footer from "@/components/Footer.vue";
+import CatalogeMob from "@/components/home/CatalogeMob.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+	 Search,
+	 Interest,
+	 Events,
+	 Footer,
+    "v-cataloge": Cataloge,
+    CatalogeMob
+  }
+};
+</script>
+<style lang="scss">
+</style>
